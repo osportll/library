6546f6b57c0b60253a1599ca3f827bdd500416e4
Oscar <oscarportillo721@gmail.com>
2023-08-02 05:36:23 -0300

    Finished iving style to the book cards. Made a grid for the cards container so it's more responsive when resizin the window. Updated the displayBook() function: made it so when the function is called, the book card HTML elements are created from scratch with their classes. Created a forEach loop for the myLibrary array, so the function loops through the object properties and edits the text content and classes of the cards accordingly. Created a counter variable 'submitIsClicked' so when the create book button on the modal is clicked, the counter increases. Created an if statement inside the displayBook() function stating that if the counter is higher than 1, it should delete the first element of the array; this was done to avoid cards duplication when calling the displayBook() function.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const pages = document.querySelector('#pages');
-const hasReadBook = document.querySelector('#hasRead');
 const btn = document.querySelector('.btn');
 const modalContainer = document.querySelector('.modalContainer');
 const bookForm = document.querySelector('.bookForm');
 const overlay = document.querySelector('.overlay');
+const cards = document.querySelector('.cards');
+
+let submitIsClicked = 0;
 
 let myLibrary = [];
 
@@ -20,17 +19,52 @@ function addBookToLibrary(bookTitle, bookAuthor, bookPages, hasReadTheBook) {
   const newBook = new Book(bookTitle, bookAuthor, bookPages, hasReadTheBook);
 
   myLibrary.push(newBook);
+
   displayBook();
+
   return newBook;
 }
 
 function displayBook() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    title.textContent = myLibrary[i].title;
-    author.textContent = myLibrary[i].author;
-    pages.textContent = myLibrary[i].pages;
-    hasReadBook.textContent = myLibrary[i].hasReadBook;
+  if (submitIsClicked > 1) {
+    myLibrary.shift();
   }
+
+  const newDiv = document.createElement('div');
+  const newP1 = document.createElement('p');
+  const newP2 = document.createElement('p');
+  const newP3 = document.createElement('p');
+  const createBtn1 = document.createElement('button');
+  const createBtn2 = document.createElement('button');
+  newP1.classList.add('title');
+  newP2.classList.add('author');
+  newP3.classList.add('pages');
+  createBtn1.classList.add('hasRead');
+  newDiv.classList.add('bookCard');
+
+  const elementsToAdd = [newP1, newP2, newP3, createBtn1, createBtn2];
+
+  elementsToAdd.forEach((element) => {
+    newDiv.appendChild(element);
+  });
+
+  cards.appendChild(newDiv);
+
+  myLibrary.forEach((book) => {
+    newP1.textContent = book.title;
+    newP2.textContent = book.author;
+    newP3.textContent = book.pages;
+    createBtn2.classList.add('showRemoveBtn');
+    createBtn2.textContent = 'Remove';
+
+    if (book.hasReadBook === 'Already Read') {
+      createBtn1.classList.add('showReadBtn');
+      createBtn1.textContent = 'Already read';
+    } else if (book.hasReadBook === 'Not read yet') {
+      createBtn1.classList.add('showNotReadBtn');
+      createBtn1.textContent = 'Not read yet';
+    }
+  });
 }
 
 btn.addEventListener('click', () => {
@@ -41,6 +75,8 @@ btn.addEventListener('click', () => {
 bookForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
+  submitIsClicked++;
+  console.log(submitIsClicked);
   const bookTitle = document.querySelector('.title').value;
   const bookAuthor = document.querySelector('.author').value;
   const bookPages = document.querySelector('.pages').value;
@@ -53,7 +89,12 @@ bookForm.addEventListener('submit', (e) => {
     bookIsRead = 'Not read yet';
   }
 
-  addBookToLibrary(bookTitle, bookAuthor, bookPages, bookIsRead);
+  addBookToLibrary(
+    `"${bookTitle}"`,
+    bookAuthor,
+    `${bookPages} pages`,
+    bookIsRead
+  );
 
   console.log(myLibrary);
   modalContainer.classList.remove('showModal');
@@ -70,6 +111,5 @@ overlay.addEventListener('click', () => {
 /* Things to add:
 
 -Make the pages input accept only numbers
--Make the question input a checkbox instead
 
 */
